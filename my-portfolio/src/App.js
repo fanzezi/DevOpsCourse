85e02bb6c6a33e555a11cb0b2f28c131f6018923
Fanny Andersson <fanny.andersson@cybercom.com>
2022-03-07 20:15:00 +0100

    UI added

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'animate.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. yoo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <div className="Wrapper" >
+          <h1 className="animate__animated animate__bounce">Hej!</h1>
+          <div className="Circle2" />
+          <div className="Circle3" />
+          <div className="Circle4" />
+          <div className="Circle5" />
+        </div>
+      <div className="infoBox">Här kan det stå rolig info</div>
       </header>
     </div>
   );
